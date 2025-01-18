61a74457adbf9f7acdd542055c563056f673b7c7
Akhil <akhilr222@gmail.com>
2025-01-18 18:21:32 +0530

    desc

--- src/components/home/AdvertisementCards.jsx
+++ src/components/home/AdvertisementCards.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AdvertisementCards = ({item}) => {
   return (
+    <Link to={`/ad/${item.id}`}>
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
                     <img src={item.image} alt="Product" className="w-full h-48 object-cover" />
                     <div className="p-4">
@@ -10,6 +12,7 @@ const AdvertisementCards = ({item}) => {
                         <div className="text-pink-600 font-bold text-lg">${item.price}</div>
                     </div>
                 </div>
+                </Link>
   )
 }
 
